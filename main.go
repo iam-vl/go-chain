2bcb335a4d711c21d660bd72764e9424da84f537
vl <gztrk@engineer.com>
2024-10-16 00:01:22 +0300

    create wallet

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-chain/wallet"
 	"log"
 )
 
@@ -10,20 +11,28 @@ func init() {
 }
 
 func main() {
-	myBlockchainAddress := "my_blockchain_address"
-	blockchain := NewBlockchain(myBlockchainAddress)
-	blockchain.Print()
-
-	blockchain.AddTransaction("A", "B", 1.0)
-	blockchain.Mining()
-	blockchain.Print()
-
-	blockchain.AddTransaction("C", "D", 2.0)
-	blockchain.AddTransaction("X", "Y", 3.0)
-	blockchain.Mining()
-	blockchain.Print()
-
-	fmt.Printf("MY: %.1f\n", blockchain.CalculateTotalAmount("my_blockchain_address"))
-	fmt.Printf("C: %.1f\n", blockchain.CalculateTotalAmount("C"))
-	fmt.Printf("D: %.1f\n", blockchain.CalculateTotalAmount("D"))
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKey())
+	fmt.Println(w.PublicKey())
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
 }
+
+// func mainBcp() {
+// 	myBlockchainAddress := "my_blockchain_address"
+// 	blockchain := NewBlockchain(myBlockchainAddress)
+// 	blockchain.Print()
+
+// 	blockchain.AddTransaction("A", "B", 1.0)
+// 	blockchain.Mining()
+// 	blockchain.Print()
+
+// 	blockchain.AddTransaction("C", "D", 2.0)
+// 	blockchain.AddTransaction("X", "Y", 3.0)
+// 	blockchain.Mining()
+// 	blockchain.Print()
+
+// 	fmt.Printf("MY: %.1f\n", blockchain.CalculateTotalAmount("my_blockchain_address"))
+// 	fmt.Printf("C: %.1f\n", blockchain.CalculateTotalAmount("C"))
+// 	fmt.Printf("D: %.1f\n", blockchain.CalculateTotalAmount("D"))
+// }
